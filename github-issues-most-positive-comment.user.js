6781fa65cbbdf7b945d4ee754812202f9e65cc7c
Nikhil Pallamreddy <nikhil.pallamreddy+git@gmail.com>
2021-10-07 16:29:22 +0530

    Clean up `gilted` code

--- github-issues-most-positive-comment.user.js
+++ github-issues-most-positive-comment.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name          GitHub Gilted Comment
+// @name          Gilted
 // @description   Scroll to comment with most positive reactions on a GitHub issue
 // @author        npalladium
 // @copyright     npalladium
-// @version       0.0.2
+// @version       0.0.1
 // @namespace     https://github.com/npalladium/userscripts
 // @license       MIT; https://choosealicense.com/licenses/mit/
 // @match         https://github.com/*/issues/*
@@ -12,21 +12,25 @@
 // ==/UserScript==
 
 
-
-(function(){
+/**
+* @listens popstate
+*/
+(function () {
   "use strict";
-  function scrollToGildedComment() {
+
+  function parseToIntOrZero(str) {
+    return isNaN(str) ? 0 : parseInt(str);
+  }
+
+  /**
+   * @description
+   * Finds the comment with most positive reactions and scrolls to it
+   * @remarks
+   * gilted roughly means covered with gold
+   * GILTED also stands for GitHub Issue Lookup Through Emoji Directions
+   */
+  function scrollToGiltedComment() {
     "use strict";
-    const REACTIONS = [
-      "THUMBS_UP",
-      "THUMBS_DOWN",
-      "LAUGH",
-      "HOORAY",
-      "CONFUSED",
-      "HEART",
-      "ROCKET",
-      "EYES",
-    ];
     const POSITIVE_REACTIONS = [
       "THUMBS_UP",
       "LAUGH",
@@ -34,34 +38,29 @@
       "HEART",
       "ROCKET",
     ];
-    const NEGATIVE_REACTIONS = ["THUMBS_DOWN", "CONFUSED"];
 
     const COMMENT_SELECTOR = ".js-comment";
-    const REACTION_SELECTOR = ".reaction-summary-item";
-
-    const parseToInt = str => isNaN(str) ? 0 : parseInt(str);
+    const COUNT_SELECTOR = "g-emoji+span";
 
     const comments = [...document.querySelectorAll(COMMENT_SELECTOR)];
     let positiveReactions = [];
     for (let comment of comments) {
       let count = POSITIVE_REACTIONS.reduce(
           (sum, val) =>
-              sum + parseToInt(comment.querySelector(`[value~=${val}] g-emoji+span`)?.innerHTML),
+              sum +
+              parseToIntOrZero(
+                  comment.querySelector(`[value~=${val}] ${COUNT_SELECTOR}`)
+                      ?.innerHTML
+              ),
           0
       );
       positiveReactions.push(count);
     }
-    const maxPositiveIndex = positiveReactions.indexOf(Math.max(...positiveReactions));
+    const maxPositiveIndex = positiveReactions.indexOf(
+        Math.max(...positiveReactions)
+    );
     comments[maxPositiveIndex].scrollIntoView();
   }
-  scrollToGildedComment();
-  window.addEventListener('popstate', scrollToGildedComment());
-  let lastUrl = location.href;
-  new MutationObserver(() => {
-    const url = location.href;
-    if (url !== lastUrl) {
-      lastUrl = url;
-      // scrollToGildedComment();
-    }
-  }).observe(document, {subtree: true, childList: true});
+  scrollToGiltedComment();
+  window.addEventListener("popstate", scrollToGiltedComment());
 })();
